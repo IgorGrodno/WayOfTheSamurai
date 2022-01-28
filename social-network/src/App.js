4d6c9ee4e280123234a02c0f7b3aa2b240c29e84
IgorGrodno <37707678+IgorGrodno@users.noreply.github.com>
2022-01-28 19:49:21 +0300

    add HTML and CSS

--- social-network/src/App.js
+++ social-network/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wraper">
+      <div className="app-grid">
+        <header className="header">
+          <img
+            src="https://cdn.logo.com/hotlink-ok/logo-social.png"
+            alt="logo"
+            className="logo-img"
+          />
+        </header>
+
+        <nav className="nav">
+          <ul>
+            <li>
+              <a>Profile</a>
+            </li>
+            <li>
+              <a>Massages</a>
+            </li>
+            <li>
+              <a>News</a>
+            </li>
+            <li>
+              <a>Music</a>
+            </li>
+            <li>
+              <a>Setings</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="main-content">
+          <div className="main-img main-content-item">
+            <img
+              src="https://klike.net/uploads/posts/2019-05/1556708032_1.jpg"
+              alt="main-img"
+            />
+          </div>
+          <div className="avatar-and-description main-content-item">
+            <div className="avatar-container">
+              <img
+                src="https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg"
+                alt="avatar"
+              />
+            </div>
+            <div className="description-container">
+              <div className="name">Jone Smith</div>
+              <div className="description">
+                <p>21 yers old</p>
+                <p>agent</p>
+              </div>
+            </div>
+          </div>
+          <div className="my-posts-container">
+            <div className="my-posts-label">My posts</div>
+            <div className="my-posts-textarea-container">
+              <textarea id="post-text" className="post-textarea"></textarea>
+            </div>
+            <div className="send-button-container">
+              <button id="post-send-button" className="post-send-button">
+                Send
+              </button>
+            </div>
+            <div className="posts-list-container">
+              <div className="post">post 0</div>
+              <div className="post">post 1</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
